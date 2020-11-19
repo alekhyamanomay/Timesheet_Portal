2408780214a44a20f940064ee29875a44429663a
farooq sheikh <57211933+unofficialfarooqsheikh@users.noreply.github.com>
2020-11-19 18:14:44 +0530

    Update users.py

--- portal/models/users.py
+++ portal/models/users.py
@@ -9,13 +9,13 @@ class User(db.Model):
     Password = db.Column(db.String(150), nullable=False)
     UserName = db.Column(db.String(50), unique=False, nullable=False)	
     Email	= db.Column(db.String(50), unique=True, nullable=False)
-    Permission	= db.Column(db.String(20), unique=False, nullable=False)
     Manager	 = db.Column(db.String(50), unique=False, nullable=True)
     ManagerEmail  = db.Column(db.String(50), unique=False, nullable=True)
     SecondaryManager = db.Column(db.String(50), unique=False, nullable=True)
     SecondaryManagerEmail = db.Column(db.String(50), unique=False, nullable=True)
     Role = db.Column(db.String(150), nullable=False)
     Status = db.Column(db.String(150), nullable=False)
+    TemporaryPassword	= db.Column(db.Boolean, unique=False, nullable=False)
     
     timesheet_entry = db.relationship('TimesheetEntry', backref='User', lazy=True)
   
